6d068e6bab4a375f2e1af1643ab4069fc946ece9
yolona-oss <siisgoo@mail.ru>
2022-07-29 18:55:43 +0300

    modified:   src/lib/data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -8,3 +8,26 @@ export function getDataByPath(obj: object, path: string): any {
 
         return ret
 }
+
+export function assign(obj: object, _prop: string, value: any) {
+        let prop = new Array<string>()
+        if (typeof _prop === "string")
+                prop = _prop.split(".")
+
+        if (prop.length > 1) {
+                var e = prop.shift()
+                // @ts-ignore
+                this.assign(obj[e] =
+                        // @ts-ignore
+                        Object.prototype.toString.call(obj[e]) === "[object Object]"
+                        // @ts-ignore
+                        ? obj[e]
+                        : {},
+                        prop.join('.'),
+                        value)
+        } else {
+                // @ts-ignore
+                obj[prop[0]] = value
+        }
+        return obj
+}
